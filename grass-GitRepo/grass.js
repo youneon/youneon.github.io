feecbf36020c4de119f4bb6f010207e5cb0c5ece
jeonghye-youn <breezeshawl@gmail.com>
2019-11-07 00:53:16 +0900

    Update grass.js

--- grass-GitRepo/grass.js
+++ grass-GitRepo/grass.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('.bar:hover').bind('touchstart touchend', function(e) {
+    $('.bar').bind('touchstart touchend', function(e) {
         e.preventDefault();
         $(this).toggleClass('hover_effect');
     });
