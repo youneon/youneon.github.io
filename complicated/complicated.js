80941d6607be89803795a69dadd63d19693ebe01
jeonghye-youn <breezeshawl@gmail.com>
2019-11-09 00:50:54 +0900

    Update complicated.js

--- complicated/complicated.js
+++ complicated/complicated.js
@@ -14,6 +14,10 @@ $(function(){
 $(document).ready(function() {
   $('.block').bind('touchstart touchend touchmove', function(e) {
       e.preventDefault();
-      $(this).toggleClass('off');
+      var touches = evt.changedTouches;
+
+    for (var i = 0; i < touches.length; i++) {
+        $(this).toggleClass('off');
+    }
   });
 });
