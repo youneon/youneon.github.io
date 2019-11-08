24593e380e70e551ab07429d5f8dd8d663a77f49
jeonghye-youn <breezeshawl@gmail.com>
2019-11-09 01:06:55 +0900

    Update complicated.js

--- complicated/complicated.js
+++ complicated/complicated.js
@@ -11,25 +11,34 @@ $(function(){
 });
 
 
+// $(document).ready(function() {
+//   // $('.block').bind('touchstart', function(e) {
+//   //     e.preventDefault();
+//   //     var touches = e.changedTouches;
+
+//   //   for (var i = 0; i < touches.length; i++) {
+//   //       $(this).toggleClass('off');
+//   //   }
+//   // });
+
+
+//   $('.block').bind('touchmove', function(e) {
+//     //e.preventDefault();
+//     var touches = e.changedTouches;
+
+//     for (var i = 0; i < touches.length; i++) {
+//       e.preventDefault();
+//         $(this).toggleClass('off');
+//     }
+//   });
+
+// });
+
+
+
 $(document).ready(function() {
-  // $('.block').bind('touchstart', function(e) {
-  //     e.preventDefault();
-  //     var touches = e.changedTouches;
-
-  //   for (var i = 0; i < touches.length; i++) {
-  //       $(this).toggleClass('off');
-  //   }
-  // });
-
-
-  $('.block').bind('touchmove', function(e) {
-    //e.preventDefault();
-    var touches = e.changedTouches;
-
-    for (var i = 0; i < touches.length; i++) {
+  $('.block').bind('touchstart touchend touchmove', function(e) {
       e.preventDefault();
-        $(this).toggleClass('off');
-    }
+      $(this).toggleClass('off');
   });
-
 });
